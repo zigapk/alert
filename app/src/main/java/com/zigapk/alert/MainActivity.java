8067e2acc9cafc4f8a40c06ad035b2c330a07051
Žiga Patačko Koderman <ziga.patacko@gmail.com>
2016-07-31 19:55:31 +0200

    add service and broadcast receiver to enable background work

--- app/src/main/java/com/zigapk/alert/MainActivity.java
+++ app/src/main/java/com/zigapk/alert/MainActivity.java
@@ -1,25 +1,28 @@
 package com.zigapk.alert;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.zigapk.alert.utils.Util;
 
+import java.util.Calendar;
+
 public class MainActivity extends AppCompatActivity {
+
+    MyReceiver alarm = new MyReceiver();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-    }
-
-    public void turnOn(View v){
-        Util.turnGPSOn(getApplicationContext());
-    }
-
-    public void turnOff(View v){
-        Util.turnGPSOff(getApplicationContext());
+        alarm.cancelAlarm(getApplicationContext());
+        alarm.setAlarm(getApplicationContext());
     }
 }
